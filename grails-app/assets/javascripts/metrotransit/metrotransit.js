f622428ad12db98058f21af76b2f2e3097c27e3e
Saravana Kumar <saravana.kumar@meygam.io>
2016-04-10 20:17:54 -0500

    Initial commit

---
+++ grails-app/assets/javascripts/metrotransit/metrotransit.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /metrotransit/core/metrotransit.core
+//= require /metrotransit/index/metrotransit.index
+
+angular.module("metrotransit", [
+    "metrotransit.core",
+    "metrotransit.index"
+]);
